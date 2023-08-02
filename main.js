f63fe50277d74a61a231ad536bb2499a890c35e7
SeonhyeY <syoon330@gmail.com>
2023-08-01 21:22:21 -0600

    Add function to draw lines

---
+++ main.js
@@ -0,0 +1,43 @@
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d'); // brush
+const CANVAS_WIDTH = 800;
+const CANVAS_HEIGHT = 800;
+
+canvas.width = CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT;
+
+canvas.addEventListener('click', onCanvasClick);
+canvas.addEventListener('mousemove', onMove);
+canvas.addEventListener('mousedown', startPainting);
+canvas.addEventListener('mouseup', cancelPainting);
+canvas.addEventListener('mouseleave', cancelPainting);
+
+let isPainting = false;
+let isFilling = false;
+
+function onCanvasClick() {
+  if (!isFilling) {
+    return;
+  } else {
+    ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    filledColor = ctx.fillStyle;
+  }
+}
+
+function onMove(event) {
+  if (isPainting) {
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+    return;
+  }
+  ctx.moveTo(event.offsetX, event.offsetY);
+}
+
+function startPainting() {
+  isPainting = true;
+}
+
+function cancelPainting() {
+  isPainting = false;
+  ctx.beginPath();
+}
